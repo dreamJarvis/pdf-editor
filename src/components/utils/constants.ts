d0af7f54333e19799d1a5c23081856e6bc8e2ff2
Tarun Jha <jhatarun16@gmail.com>
2024-04-20 02:21:17 +0530

    changed pdf view formal

--- src/components/utils/constants.ts
+++ src/components/utils/constants.ts
@@ -64,5 +64,5 @@ export const html2pdfOptions = {
 	margin: 1,
 	image: { type: "jpeg", quality: 0.98 },
 	html2canvas: { scale: 2 },
-	jsPDF: { unit: "in", format: "a2", orientation: "p" },
+	jsPDF: { unit: "in", format: "a1", orientation: "p" },
 };
