ae4695cc9842c17c6bf56994ff6ea9e9c5d0d3f1
Tarun Jha <jhatarun16@gmail.com>
2024-04-21 03:34:32 +0530

    moved html2pdf.d.ts to utils

--- src/components/resume/view/ConvertToPDF.tsx
+++ src/components/resume/view/ConvertToPDF.tsx
@@ -4,21 +4,27 @@ import { Button, Container } from "@mui/material";
 import html2pdf from "html2pdf.js";
 import { useRef } from "react";
 import { ViewResume } from "./ViewResume";
-import { html2pdfOptions } from '../../utils/constants';
+import { html2pdfOptions } from "../../utils/constants";
 
 export const ConvertToPDF = () => {
-   const contentRef = useRef(null);
-   const convertToPdf = () => {
-      const content = contentRef.current;
-      html2pdf().set(html2pdfOptions).from(content).save();
-   };
+	const contentRef = useRef(null);
+	const convertToPdf = () => {
+		const content = contentRef.current;
+		html2pdf().set(html2pdfOptions).from(content).save();
+	};
 
-   return (
-      <Container className='bg-red-100'>
-         <ViewResume contentRef={contentRef} />
-         <div className='mb-10 bg-red-100'>
-            <Button variant="contained" color="success" size="large" onClick={convertToPdf}>Convert to PDF</Button>
-         </div>
-      </Container>
-   );
+	return (
+		<Container className='bg-red-100'>
+			<ViewResume contentRef={contentRef} />
+			<div className='mb-10 bg-red-100'>
+				<Button
+					variant='contained'
+					color='success'
+					size='large'
+					onClick={convertToPdf}>
+					Convert to PDF
+				</Button>
+			</div>
+		</Container>
+	);
 };
